38a8fa07169402654a1b4a5e7ba9c2d5c0f975c3
LucasNS18 <lucasnunezzz602@gmail.com>
2024-10-08 09:33:49 -0300

    new release

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,4 +1,4 @@
-//DESIGN: FINALIZADO 
+//DESIGN: FINALIZADO
 //FUNCIONALIDADES: NÃO FINALIZADO
 
 import 'package:flutter/material.dart';
@@ -7,7 +7,7 @@ import 'package:app_flutter/widgets/gradient_background.dart';
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
-  @override 
+  @override
   State<SignUpPage> createState() => _SignUpPageState();
 }
 
@@ -15,9 +15,9 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget customTextField(String labelText, String hintText, bool obscureText) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SizedBox(
-          height: 50,
-          child: TextField(
+      child: SizedBox(
+        height: 50,
+        child: TextField(
           obscureText: obscureText,
           decoration: InputDecoration(
             labelText: labelText,
@@ -26,7 +26,8 @@ class _SignUpPageState extends State<SignUpPage> {
             hintText: hintText,
             hintStyle: TextStyle(color: Color(0xFFFFFFFF)),
             focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFFC99856),
+              borderSide: BorderSide(
+                color: Color(0xFFC99856),
               ),
             ),
             enabledBorder: OutlineInputBorder(
@@ -34,7 +35,7 @@ class _SignUpPageState extends State<SignUpPage> {
               borderRadius: BorderRadius.circular(12),
             ),
           ),
-          style: TextStyle (
+          style: TextStyle(
             color: Color(0xFFFFFFFF),
             fontSize: 18,
           ),
@@ -56,32 +57,35 @@ class _SignUpPageState extends State<SignUpPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/images/logo.png', 
-                  width: screenWidth, 
-                  height: screenHeight * 0.4, 
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: screenWidth,
+                  height: screenHeight * 0.4,
                   fit: BoxFit.cover,
                 ),
 
                 //TÍTULO SIGN UP
                 const SizedBox(height: 24),
                 Padding(
-                  padding: const EdgeInsets.only(left:24),
+                  padding: const EdgeInsets.only(left: 24),
                   child: Row(
                     children: [
                       Icon(
-                        Icons.arrow_back_ios_new, 
-                        color: Color(0xFFC99856),),
+                        Icons.arrow_back_ios_new,
+                        color: Color(0xFFC99856),
+                      ),
                       SizedBox(width: 18),
-                        Text('Sign Up',
-                          style: TextStyle(
-                            fontFamily: 'InriaSerif',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            color: Color(0xFFC99856),
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontFamily: 'InriaSerif',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Color(0xFFC99856),
                         ),
                       ),
                     ],
-                  ), 
+                  ),
                 ),
 
                 const SizedBox(height: 24),
@@ -96,38 +100,40 @@ class _SignUpPageState extends State<SignUpPage> {
                 const SizedBox(height: 18),
                 customTextField('Confirmação', 'Confirme sua senha', true),
 
-              //BUTTON SIGN UP
-              const SizedBox(height: 24),
-              Padding(
-                padding: EdgeInsets.only(right: 24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      height: 42,
-                      width: 138,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xC8000000),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
+                //BUTTON SIGN UP
+                const SizedBox(height: 24),
+                Padding(
+                  padding: EdgeInsets.only(right: 24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 42,
+                        width: 138,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xC8000000),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
                           ),
-                        ),
-                        onPressed: () {},
-                        child: Text('Sign Up',
-                          style: TextStyle(
-                            fontFamily: 'InriaSerif',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Color(0xFFC99856),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontFamily: 'InriaSerif',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Color(0xFFC99856),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
